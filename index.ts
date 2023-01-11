5482212b427ec1bfb7d696718d69d677604b0663
Thomashq <thomashenriqq@gmail.com>
2023-01-10 21:23:56 -0300

    adição de react no projeto

--- index.ts
+++ index.ts
@@ -1,29 +1,22 @@
-const {app, BrowserWindow} = require('electron');
 const express = require('express');
 const mongoose = require('mongoose')
+const routes = require('./src/router/routers.ts')
 const server = express();
-let mainWindow;
 
 server.use(
     express.urlencoded({
         extended: true
     })
 )
-server.use(express.json())
+
 mongoose
-    .connect('mongodb://localhost:27017')
-    .then(() => {
-        server.listen(3000)
-        console.log("Conectado com sucesso")
-    })
-    .catch((err) => console.log(err))
-
-app.on('ready', () => {
-
-    mainWindow = new BrowserWindow({
-        
-    });
-    
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
-
+.connect('mongodb://localhost:27017')
+.then(() => {
+    server.listen(3000)
+    console.log("Conectado com sucesso")
+    console.log(`Servidor está rodando na porta ${3000}`)
 })
+.catch((err) => console.log(err))
+ 
+server.use(express.json())
+server.use('/clientes', routes)
