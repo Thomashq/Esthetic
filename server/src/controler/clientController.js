3cd1239e452a8b30baa82368ec45cb9b7023002a
Paulo Gonçalves <guigui041201@gmail.com>
2023-01-19 22:14:19 -0300

    Cria lista inicial de clientes

--- server/src/controler/clientController.js
+++ server/src/controler/clientController.js
@@ -1,70 +1,76 @@
-const expressRoute = require('express')
-const clientModelRoutes = require('../model/Client.js')
+const expressRoute = require("express");
+const clientModelRoutes = require("../model/Client.js");
 
-const router = expressRoute.Router()
+const router = expressRoute.Router();
 
 //rota de get
-router.get('/getClient', async (req, res) => {
-    try{
-        const client = await clientModelRoutes.find()
-        res.status(200).json(client)
-    }catch(err){
-        res.status(500).json({error: err})
-    }
-})
+router.get("/getClient", async (req, res) => {
+  try {
+    const client = await clientModelRoutes.find();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).json(client);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
-router.get('/getClientByName', async(req, res) => {
-    
-    const name = req.body.Name
-    try{
-        const client = await clientModelRoutes.findOne({Name: name})
-        res.json(client)
-    }
-    catch(err){
-        res.status(500).json({error:err})
-    }
-})
+router.get("/getClientByName", async (req, res) => {
+  const name = req.body.Name;
+  try {
+    const client = await clientModelRoutes.findOne({ Name: name });
+    res.json(client);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
-router.get('/getClientById', async(req, res) => {
-    
-    const id = req.body._id
-    try{
-        const client = await clientModelRoutes.findOne({_id: id})
-        res.json(client)
-    }
-    catch(err){
-        res.status(500).json({error:err})
-    }
-})
+router.get("/getClientById", async (req, res) => {
+  const id = req.body._id;
+  try {
+    const client = await clientModelRoutes.findOne({ _id: id });
+    res.json(client);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
 //entrada de dados
-router.post('/insertNewClient', async(req, res)=>{
-    const { Address, Age, Birthdate, Cellphone, Cep, City, District, Mail, Name, Profession, State} = req.body
-    const client = {
-        Address, 
-        Age, 
-        Birthdate, 
-        Cellphone, 
-        Cep, 
-        City, 
-        District, 
-        Mail, 
-        Name, 
-        Profession, 
-        State
-    }
-    if(!Name){
-        res.status(422)
-    }
-    try{
-        
-        await clientModelRoutes.create(client)
-        res.status(201).json({message: 'cliente criado'})
-    
-    }catch(err){
-        res.status(500).json({error: err})
-    }
-})
+router.post("/insertNewClient", async (req, res) => {
+  const {
+    Address,
+    Age,
+    Birthdate,
+    Cellphone,
+    Cep,
+    City,
+    District,
+    Mail,
+    Name,
+    Profession,
+    State,
+  } = req.body;
+  const client = {
+    Address,
+    Age,
+    Birthdate,
+    Cellphone,
+    Cep,
+    City,
+    District,
+    Mail,
+    Name,
+    Profession,
+    State,
+  };
+  if (!Name) {
+    res.status(422);
+  }
+  try {
+    await clientModelRoutes.create(client);
+    res.status(201).json({ message: "cliente criado" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
 
 module.exports = router;
- 
