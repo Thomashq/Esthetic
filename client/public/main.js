52e5a67b9b02e0d22b98c9a720f10dac0cc9f78a
thomashenriq <thomashenriqq@gmail.com>
2023-01-29 17:33:52 -0300

    Aplicativo buildando

--- client/public/main.js
+++ client/public/main.js
@@ -1,26 +1,34 @@
-const { app, BrowserWindow } = require("electron");
-//require('@electron/remote/main').initialize()
-
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-  win.loadURL("http://localhost:3000");
-  //remove o menu superior
-  //win.removeMenu();
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, './index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadURL(startUrl);
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    });
 }
-
-app.whenReady().then(() => {
-  createWindow();
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
-
-// app.on('window-all-closed', function(){
-//     if(process.platform !== 'darwin'){
-//         app.quit()
-//     }
-// })
-
-// app.on('activate', function(){
-//     if(BrowserWindow.getAllWindows().length === 0) createWindow()
-// })
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+});
